390ee09c70cb8a2bc6bd2bb0fe3f5daf16ca24f8
busraderbazlar <busra.derbazlar@gmail.com>
2022-09-11 15:35:16 +0300

    11092022

---
+++ Alıştırmalar/test.py
@@ -0,0 +1,40 @@
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string * how_many_snakes)
+
+"""names = input("Enter names separated by commas: ").title().split(",")
+assignments = input("Enter assignment counts separated by commas: ").split(",")
+grades = input("Enter grades separated by commas: ").split(",")
+
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+submit before you can graduate. You're current grade is {} and can increase \
+to {} if you submit all assignments before the due date.\n\n"
+
+for name, assignment, grade in zip(names, assignments, grades):
+    print(message.format(name, assignment, grade, int(grade) + int(assignment)*2))"""
+
+
+names = input("Enter names separated by commas: ").title().split(",")
+assignments = input("Enter assignment counts separated by commas: ").split(",")
+grades = input("Enter grades separated by commas: ").split(",")
+
+message = f"Hi {names},\n\nThis is a reminder that you have {assignments} assignments left to \
+submit before you can graduate. You're current grade is {(grades)} and can increase \
+to {(int(grade) + int(assignment)*2)} if you submit all assignments before the due date.\n\n"
+
+print (message)
+
+# write a for loop that iterates through each set of names, assignments, and grades to print each student's message
